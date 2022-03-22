0ec882164fe6c2d9b94bc0c2dc30796b03c2f637
atshibko <atshibko@gmail.com>
2022-03-22 15:59:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinesNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesAmount = 52;
+            int imagesInLine = 3;
+
+            Console.WriteLine("Полных строк: {0}\nОсталось элементов: {1}", (int)(imagesAmount / imagesInLine), imagesAmount % imagesInLine);
+            Console.ReadKey();
+        }
+    }
+}
